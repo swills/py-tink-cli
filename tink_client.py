84073f5dc46079149d1a6fe892886af79a413d71
Steve Wills <steve@mouf.net>
2021-12-23 12:20:33 -0500

    add workflow push, rename some things

--- tink_client.py
+++ tink_client.py
@@ -22,6 +22,30 @@ def get_hostname_for_mac(server, port, creds, mac):
     return response.network.interfaces[0].dhcp.hostname
 
 
+def get_mac_for_hostname(server, port, creds, hostname):
+    res = get_all_hardware(server, port, creds)
+    mac = ""
+    for re in res:
+        if re['hostname'] == hostname:
+            mac = re['mac']
+    return mac
+
+
+def push_workflow(server, port, creds, client_name, template_name):
+    result = []
+    with grpc.secure_channel(server + ":" + port, creds) as channel:
+        stub = workflow_pb2_grpc.WorkflowServiceStub(channel)
+        client_mac = get_mac_for_hostname(server, port, creds, client_name)
+        template_id = get_template_by_name(server, port, creds, template_name)
+        # hardware = '{"device_1":"'${MAC}'"}'
+        hardware = {'device_1': client_mac}
+        hardware_json = json.dumps(hardware)
+        response = stub.CreateWorkflow(workflow_pb2.CreateRequest(
+            template=template_id, hardware=hardware_json))
+        result = [response.id]
+    return result
+
+
 def get_all_workflows(server, port, creds):
     with grpc.secure_channel(server + ":" + port, creds) as channel:
         stub = workflow_pb2_grpc.WorkflowServiceStub(channel)
@@ -96,7 +120,17 @@ def get_template_by_id(server, port, creds, template_id):
     result = {}
     for re in res:
         if re['id'] == template_id:
-            return re
+            result = re
+    return result
+
+
+def get_template_by_name(server, port, creds, template_name):
+    res = get_all_templates(server, port, creds)
+    result = {}
+    for re in res:
+        if re['name'] == template_name:
+            result = re['id']
+    return result
 
 
 def get_template_steps(server, port, creds, template_id):
@@ -115,8 +149,8 @@ def run():
                         dest='debug_logs',
                         help='debug Output'
                         )
-    parser.add_argument("--host",
-                        dest="host",
+    parser.add_argument("--tink_host",
+                        dest="tink_host",
                         default=os.environ.get('TINK_HOST'),
                         help="tink host. required.")
     parser.add_argument("--rpc_port",
@@ -129,14 +163,23 @@ def run():
                         help="http port. Default is '42114'.")
     parser.add_argument("--template_name",
                         dest="template_name",
+                        default=None,
                         help="template name to operate on")
+    parser.add_argument("--host_name",
+                        dest="host_name",
+                        default=None,
+                        help="host name to operate on")
     parser.add_argument("action",
                         help="action to perform")
     parser.add_argument("object",
                         help="what to operate on")
     args = parser.parse_args()
 
-    cert_url = 'http://' + args.host + ':' + args.http_port + '/cert'
+    if args.tink_host is None:
+        print("TINK_HOST environment variable must be set or --host must be specified")
+        return
+
+    cert_url = 'http://' + args.tink_host + ':' + args.http_port + '/cert'
     with urllib.request.urlopen(cert_url) as response:
         trusted_certs = response.read()
 
@@ -144,23 +187,30 @@ def run():
 
     if args.action == "get":
         if args.object == "workflows":
-            result = get_all_workflows(args.host, args.rpc_port, creds)
+            result = get_all_workflows(args.tink_host, args.rpc_port, creds)
             print(json.dumps(result, indent=2))
         elif args.object == "hardware":
-            result = get_all_hardware(args.host, args.rpc_port, creds)
+            result = get_all_hardware(args.tink_host, args.rpc_port, creds)
             print(json.dumps(result, indent=2))
         elif args.object == "templates":
-            result = get_all_templates(args.host, args.rpc_port, creds)
+            result = get_all_templates(args.tink_host, args.rpc_port, creds)
             print(json.dumps(result, indent=2))
         elif args.object == "template":
             if args.template_name is not None:
-                result = get_all_templates(args.host, args.rpc_port, creds)
+                result = get_all_templates(args.tink_host, args.rpc_port, creds)
                 for template in result:
                     if template['name'] == args.template_name:
-                        id = template['id']
-                        template_result = get_template_steps(args.host, args.rpc_port,
-                                                         creds, template_id=id)
+                        template_id = template['id']
+                        template_result = get_template_steps(args.tink_host,
+                                                             args.rpc_port, creds,
+                                                             template_id=template_id)
                         print(template_result)
+    elif args.action == "push":
+        if args.object == "workflow":
+            if args.host_name is not None and args.template_name is not None:
+                result = push_workflow(args.tink_host, args.rpc_port, creds,
+                                       args.host_name, args.template_name)
+                print(json.dumps(result, indent=2))
 
 
 if __name__ == '__main__':
